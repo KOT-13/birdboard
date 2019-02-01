c0305cb2d69c284b5c398118702478085c83a1ea
alexn <anazarenco@best4u.md>
2019-02-01 14:51:18 +0200

    creating new projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -35,6 +35,14 @@ class ProjectsController extends Controller
     }
 
     /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store()
